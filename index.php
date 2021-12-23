98ab56585245915ad2a84dc08a673f0d1333842f
Kapil Kumar <kumar.kapil@dotsquares.com>
2021-12-23 16:21:41 +0530

    Commit for push code on docker

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Dotsquares 1";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Example</title>
+</head>
+<body>
+    <h1>Git working fine</h1>
+</body>
+</html>
